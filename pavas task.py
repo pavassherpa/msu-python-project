f8748ca0949234e2909c229cdee456bc5bb08bbc
pavas sherpa <pavassherpa66@gmail.com>
2023-04-18 20:10:41 +0530

    pavas task.py

---
+++ pavas task.py
@@ -0,0 +1,9 @@
+hight = float (input("enter the hight"))
+width = float (input("enter the width"))
+area = height*width
+print('the area of rectangle is', area) 
+p1=3.14
+radius=float (input("enter the radius"))
+height=float (input("enter the height"))
+volume=p1*(radius**2)height
+print('the volume of cylender is '. volume)
---
+++ pavasvs.py
@@ -0,0 +1,3 @@
+name = 'aman'
+age= 30
+print(f'{name} is {age} but still acts as a kid')
